ac9dafe62b70551e71c53337d4dbbea69ce903c7
Ezean <114819416+yjezean@users.noreply.github.com>
2024-12-15 04:57:28 +0800

    Update 2.0

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -6,25 +6,25 @@ import {
   FlagIcon,
   MapIcon,
   SparklesIcon,
-} from "@heroicons/react/24/outline";
+} from '@heroicons/react/24/outline';
 
-import GithubIcon from "../components/Icon/GithubIcon";
-import InstagramIcon from "../components/Icon/InstagramIcon";
-import LinkedInIcon from "../components/Icon/LinkedInIcon";
-import heroImage from "../images/header-background.webp";
-import porfolioImage1 from "../images/portfolio/portfolio-1.jpg";
-import porfolioImage2 from "../images/portfolio/portfolio-2.jpg";
-import porfolioImage3 from "../images/portfolio/portfolio-3.jpg";
-import porfolioImage4 from "../images/portfolio/portfolio-4.jpg";
-import porfolioImage5 from "../images/portfolio/portfolio-5.jpg";
-import porfolioImage6 from "../images/portfolio/portfolio-6.jpg";
-import porfolioImage7 from "../images/portfolio/portfolio-7.jpg";
-import porfolioImage8 from "../images/portfolio/portfolio-8.jpg";
-import porfolioImage9 from "../images/portfolio/portfolio-9.jpg";
-import porfolioImage10 from "../images/portfolio/portfolio-10.jpg";
-import porfolioImage11 from "../images/portfolio/portfolio-11.jpg";
-import profilepic from "../images/profilepic.jpg";
-import testimonialImage from "../images/testimonial.webp";
+import GithubIcon from '../components/Icon/GithubIcon';
+import InstagramIcon from '../components/Icon/InstagramIcon';
+import LinkedInIcon from '../components/Icon/LinkedInIcon';
+import heroImage from '../images/header-background.webp';
+import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
+import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
+import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
+import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
+import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
+import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
+import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
+import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import profilepic from '../images/profilepic.jpg';
+import testimonialImage from '../images/testimonial.webp';
 import {
   About,
   ContactSection,
@@ -36,31 +36,32 @@ import {
   Social,
   TestimonialSection,
   TimelineItem,
-} from "./dataDef";
+} from './dataDef';
 
 /**
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: "Takitez",
+  title: 'Takitez',
   description: "Want to know me? It's ez.",
+ 
 };
 
 /**
  * Section definition
  */
 export const SectionId = {
-  Hero: "hero",
-  About: "about",
-  Contact: "contact",
-  Portfolio: "portfolio",
-  Resume: "resume",
-  Skills: "skills",
-  Stats: "stats",
-  Testimonials: "testimonials",
+  Hero: 'hero',
+  About: 'about',
+  Contact: 'contact',
+  Portfolio: 'portfolio',
+  Resume: 'resume',
+  Skills: 'skills',
+  Stats: 'stats',
+  Testimonials: 'testimonials',
 } as const;
 
-export type SectionId = typeof SectionId[keyof typeof SectionId];
+export type SectionId = (typeof SectionId)[keyof typeof SectionId];
 
 /**
  * Hero section
@@ -71,29 +72,24 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a student pursuing for a{" "}
-        <strong className="text-stone-100">
-          Bachelor of Computer Sciences
-        </strong>
-        , at{" "}
-        <strong className="text-stone-100">Universiti Sains Malaysia</strong>.
+        I'm a student pursuing for a <strong className="text-stone-100">Bachelor of Computer Sciences</strong>, at <strong className="text-stone-100">Universiti Sains Malaysia</strong> 
+        . 
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        THis website serves as a brief introduction of myself. To know me more,
-        feel free to contact me via any available channels.
+      THis website serves as a brief introduction of myself. To know me more, feel free to contact me via any available channels.
       </p>
     </>
   ),
   actions: [
     {
-      href: "/assets/resume.pdf",
-      text: "Resume",
+      href: '/assets/resume.pdf',
+      text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
     },
     {
       href: `#${SectionId.Contact}`,
-      text: "Contact",
+      text: 'Contact',
       primary: false,
     },
   ],
@@ -104,23 +100,14 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description:
-    "As a student in Bachelor of Computer Sciences in Universiti Sains Malaysia,I have a great passion to explore job fields related to latest technologies and always passionate to explore every possibility. Take it easy and keep it easy.",
+  description: 'As a student in Bachelor of Computer Sciences in Universiti Sains Malaysia,I have a great passion to explore job fields related to latest technologies and always passionate to explore every possibility. Take it easy and keep it easy.',
   aboutItems: [
-    { label: "Location", text: "Penang, Malaysia", Icon: MapIcon },
-    { label: "Age", text: "21", Icon: CalendarIcon },
-    { label: "Nationality", text: "Malaysian", Icon: FlagIcon },
-    { label: "Interests", text: "Games, Gym, Food", Icon: SparklesIcon },
-    {
-      label: "Study",
-      text: "Universiti Sains Malaysia",
-      Icon: AcademicCapIcon,
-    },
-    {
-      label: "School",
-      text: "School of Computer Sciences",
-      Icon: BuildingOffice2Icon,
-    },
+    {label: 'Location', text: 'Penang, Malaysia', Icon: MapIcon},
+    {label: 'Age', text: '21', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Malaysian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Games, Gym, Food', Icon: SparklesIcon},
+    {label: 'Study', text: 'Universiti Sains Malaysia', Icon: AcademicCapIcon},
+    {label: 'School', text: 'School of Computer Sciences', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -129,56 +116,57 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: "Spoken languages",
+    name: 'Spoken languages',
     skills: [
       {
-        name: "Chinese",
+        name: 'Chinese',
         level: 10,
       },
       {
-        name: "English",
+        name: 'English',
         level: 8,
       },
       {
-        name: "Malay",
+        name: 'Malay',
         level: 8,
       },
     ],
   },
   {
-    name: "Programming Languages",
+    name: 'Programming Languages',
     skills: [
       {
-        name: "C++",
+        name: 'C++',
         level: 7,
       },
       {
-        name: "Python",
+        name: 'Python',
         level: 7,
       },
       {
-        name: "SQL",
+        name: 'SQL',
         level: 7,
       },
     ],
   },
   {
-    name: "Data",
+    name: 'Data',
     skills: [
       {
-        name: "Data analytic",
+        name: 'Data analytic',
         level: 8,
       },
       {
-        name: "Machine Learning",
+        name: 'Machine Learning',
         level: 7,
       },
       {
-        name: "Deep learning",
+        name: 'Deep learning',
         level: 6,
       },
     ],
   },
+  
 ];
 
 /**
@@ -186,69 +174,69 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: "Project title 1",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 1',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage1,
   },
   {
-    title: "Project title 2",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 2',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage2,
   },
   {
-    title: "Project title 3",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 3',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage3,
   },
   {
-    title: "Project title 4",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 4',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage4,
   },
   {
-    title: "Project title 5",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 5',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage5,
   },
   {
-    title: "Project title 6",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 6',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage6,
   },
   {
-    title: "Project title 7",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 7',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage7,
   },
   {
-    title: "Project title 8",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 8',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage8,
   },
   {
-    title: "Project title 9",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 9',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage9,
   },
   {
-    title: "Project title 10",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 10',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage10,
   },
   {
-    title: "Project title 11",
-    description: "Give a short description of your project here.",
-    url: "https://reactresume.com",
+    title: 'Project title 11',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
     image: porfolioImage11,
   },
 ];
@@ -258,69 +246,77 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: "October 2022 - Present",
-    location: "USM",
-    title: "Bachelor of Computer Sciences",
-    content: (
-      <p>
-        Pursuing for Degree in COmputer Sciences majoring in Intelligence
-        Computing. Graduating in October 2026.
-      </p>
-    ),
+    date: 'October 2022 - Present',
+    location: 'USM',
+    title: 'Bachelor of Computer Sciences',
+    content: <p>Pursuing for Degree in COmputer Sciences majoring in Intelligence Computing. Graduating in October 2026.</p>,
   },
   {
-    date: "2021 - 2022",
-    location: "Kolej Matrikulasi Perak",
-    title: "Matrikulasi",
-    content: (
-      <p>Taking matriculation programme in Module 1. With GPA of 4.0. </p>
-    ),
+    date: '2021 - 2022',
+    location: 'Kolej Matrikulasi Perak',
+    title: 'Matrikulasi',
+    content: <p>Taking matriculation programme in Module 1. With GPA of 4.0. </p>,
   },
   {
-    date: "2015 - 2020",
-    location: "SMK Malim Nawar",
-    title: "Secondary School",
+    date: '2015 - 2020',
+    location: 'SMK Malim Nawar',
+    title: 'Secondary School',
     content: <p>Graduate with SPM result of 10As in year 2021. </p>,
-  },
+  }
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: "October 2024 - Present",
-    location: "Advisor of Event Planning & Culture Development Department",
-    title: "Google Developer Student Clubs USM",
-    content: <p></p>,
+    date: 'October 2024 - Present',
+    location: 'Advisor of Event Planning & Culture Development Department',
+    title: 'Google Developer Student Clubs USM',
+    content: (
+      <p></p>
+    ),
   },
   {
-    date: "October 2023 - October 2024",
-    location: "Head of Event Planning & Culture Development Department",
-    title: "",
-    content: <p></p>,
+    date: 'October 2023 - October 2024',
+    location: 'Head of Event Planning & Culture Development Department',
+    title: '',
+    content: (
+      <p>
+        
+      </p>
+    ),
   },
   {
-    date: "October 2022 - October 2023",
-    location:
-      "Core team member of Event Planning & Culture Development Department",
-    title: "",
-    content: <p></p>,
+    date: 'October 2022 - October 2023',
+    location: 'Core team member of Event Planning & Culture Development Department',
+    title: '',
+    content: (
+      <p>
+        
+      </p>
+    ),
   },
   {
-    date: "2024",
-    location: "Head of department of Programme department",
-    title: "HackAttack USM X UTAR 2024",
-    content: <p></p>,
+    date: '2024',
+    location: 'Head of department of Programme department',
+    title: 'HackAttack USM X UTAR 2024',
+    content: (
+      <p></p>
+    ),
   },
   {
-    date: "2024",
-    location: "Committee of USM",
-    title: "My Universities Hackathon 2024",
-    content: <p></p>,
+    date: '2024',
+    location: 'Committee of USM',
+    title: 'My Universities Hackathon 2024',
+    content: (
+      <p></p>
+    ),
   },
   {
-    date: "2023",
-    location: "Top ten finalist as team Trisafe",
-    title: "PYDC Youth Entrepreneurship Challenge 2023",
-    content: <p></p>,
+    date: '2023',
+    location: 'Top ten finalist as team Trisafe',
+    title: 'PYDC Youth Entrepreneurship Challenge 2023',
+    content: (
+      <p></p>
+    ),
   },
 ];
 
@@ -331,12 +327,11 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: "Ezean",
-      text:
-        "As a human, just enjoy the life with the simplest mindset. Do not waste time for unhappiness. Take it easy.",
-      image:
-        "https://mondaymandala.com/wp-content/uploads/Printable-Winking-Face-Emoji_-791x1024.jpg",
+      name: 'Ezean',
+      text: 'As a human, just enjoy the life with the simplest mindset. Do not waste time for unhappiness. Take it easy.',
+      image: 'https://mondaymandala.com/wp-content/uploads/Printable-Winking-Face-Emoji_-791x1024.jpg',
     },
+    
   ],
 };
 
@@ -345,29 +340,28 @@ export const testimonial: TestimonialSection = {
  */
 
 export const contact: ContactSection = {
-  headerText: "Contact me.",
-  description:
-    "Do not hesitate to approach me if you want to know more about me. ",
+  headerText: 'Contact me.',
+  description: 'Do not hesitate to approach me if you want to know more about me. ',
   items: [
     {
       type: ContactType.Email,
-      text: "mrchongyijian@gmail.com",
-      href: "mailto:mrchongyijian@gmail.com",
+      text: 'mrchongyijian@gmail.com',
+      href: 'mailto:mrchongyijian@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: "Penang, Malaysia",
-      href: "https://maps.app.goo.gl/agDLbksMhtJ1qwTy5",
+      text: 'Penang, Malaysia',
+      href: 'https://maps.app.goo.gl/agDLbksMhtJ1qwTy5',
     },
     {
       type: ContactType.Instagram,
-      text: "@yijian.ezean",
-      href: "https://www.instagram.com/yijian.ezean/",
+      text: '@yijian.ezean',
+      href: 'https://www.instagram.com/yijian.ezean/',
     },
     {
       type: ContactType.Github,
-      text: "yjezean",
-      href: "https://github.com/yjezean",
+      text: 'yjezean',
+      href: 'https://github.com/yjezean',
     },
   ],
 };
@@ -376,15 +370,7 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  { label: "Github", Icon: GithubIcon, href: "https://github.com/yjezean" },
-  {
-    label: "LinkedIn",
-    Icon: LinkedInIcon,
-    href: "https://www.linkedin.com/in/yi-jian-chong-587653255/",
-  },
-  {
-    label: "Instagram",
-    Icon: InstagramIcon,
-    href: "https://www.instagram.com/yijian.ezean/",
-  },
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/yjezean'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/yi-jian-chong-587653255/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/yijian.ezean/'},
 ];
